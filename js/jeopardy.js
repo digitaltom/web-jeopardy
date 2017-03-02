6cfd540d0eb46c17148995d830b29ea522f8e284
Thomas Schmidt <tschmidt@suse.de>
2017-03-02 17:21:04 +0100

    fix layout

--- js/jeopardy.js
+++ js/jeopardy.js
@@ -1,4 +1,4 @@
-
+// https://etherpad.nue.suse.com/p/praxistag-jeopardy
 var dataEndpoint = "jeopardy-data.json";
 
 $.getJSON(dataEndpoint)
@@ -15,31 +15,28 @@ $.getJSON(dataEndpoint)
       category_div.fadeIn()
 
       $.each(category['questions'], function (id, question) {
-
         console.log(question)
         question_div = category_div.find('.question').first().clone()
         question_div.hide()
         question_div.removeClass('hidden')
         question_div.appendTo(category_div)
-        question_div.html(question['label'])
+        question_div.html("<b>" + question['label'] + "</b>")
         question_div.data('question', question['q'])
         question_div.data('answer', question['a'])
         question_div.fadeIn()
-
       })
-
     })
   })
 
 $(document).on('click', '.question', function(){
   console.log($(this).data('question'))
-  $( this ).removeClass('btn-primary')
+  $( this ).removeClass('btn-success')
   $( this ).addClass('btn-secondary')
-  $('#question-modal').find('.modal-body').html($(this).data('question'))
-
+  $('#question-modal').find('.modal-frage').html($(this).data('question'))
   $('#answer-modal').find('.modal-body').load($(this).data('answer'))
+})
 
-
-
-});
-
+$(document).on('click', '#answer-modal', function(){
+  $("#question-modal").modal('hide')
+  $("#answer-modal").modal('hide')
+})
